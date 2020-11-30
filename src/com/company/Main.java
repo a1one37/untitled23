80ea693de6a30e2b22416be32741a67cc5158a57
ilyamartynov989@gmail.com <ilyamartynov989@gmail.com>
2020-11-30 17:58:06 +0200

    hii mates

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello word");
         System.out.println("qq all");
+        System.out.println("hi mate");
     }
 }
